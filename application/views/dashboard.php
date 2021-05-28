eb670529340a9b4e586f89ccf63f0372b601d191
yhaae01 <suryha7fold@gmail.com>
2021-05-28 07:57:43 +0700

    menambahkan auto refresh pada dashboard admin

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,112 +1,108 @@
-        <div class="content pb-0">
-    
-        <h1><i class="fa fa-bar-chart-o"> </i> Hasil Pemilihan</h1>
-        <hr>
-        <a class="btn btn-success mb-3" href="<?php echo base_url('dasbor/export'); ?>"><i class="fa fa-print">
-        </i> Cetak</a>
+<?php
+$sec = "10";
+?>
+<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?= base_url('Dasbor')?>'">
 
-        <?php 
-        $nototal                =0;
-        $belummemilih           =0;
-        $sudahmemilih           =0;
-        $sudahabsen             =0;
-        $belumabsen             =0;
-        $sudahabsenbelummilih   =0;
-            foreach($datapemilih->result_array() as $j):
-                $id=$j['id'];
-                $suara=$j['suara'];
-                $absen=$j['absen'];
-            if ($suara!=0) {
-                $sudahmemilih++;
-            }
-            if ($suara==0) {
-                $belummemilih++;
-            }
-            if ($absen!=0){
-                $sudahabsen++;
-            }
-            if ($absen==0) {
-                $belumabsen++;
-            }
-            if ($suara==0 && $absen!=0) {
-                $sudahabsenbelummilih++;
-            };
-            $nototal++;
-            endforeach;
-            
-        ?>
-        <!-- <div class="container">
-            <div class="row">
-                <div class="col-4"> DPT : <?php echo $nototal; ?> <br> Sudah absen belum memilih : <?php echo $sudahabsenbelummilih; ?> </div>
-                <div class="col-4 text-center"> Sudah Memilih : <?php echo $sudahmemilih; ?><br>Sudah Absen : <?php echo $sudahabsen; ?></div>
-                <div class="col-4 text-right"> Belum Memilih : <?php echo $belummemilih; ?><br>Belum Absen : <?php echo $belumabsen; ?></div>
-            </div>
-        </div> -->
+    <div class="content pb-0">
 
-                <div class="row">
+    <h1><i class="fa fa-bar-chart-o"> </i> Hasil Pemilihan</h1>
+    <hr>
+    <a class="btn btn-success mb-3" href="<?php echo base_url('dasbor/export'); ?>"><i class="fa fa-print">
+    </i> Cetak</a>
 
-                    <?php $no=1;
-                        foreach($data->result_array() as $i):
-                            $id=$i['id'];
-                            $visi=$i['visi'];
-                            $misi=$i['misi']; 
-                            $namacalon=$i['namacalon']; 
-                            $foto=$i['foto']; 
-                            $totalsuara=$i['totalsuara'];        
-                    ?>
-                    <div class="col-lg-4">
-                        <aside class="profile-nav alt">
-                            <section class="card">
-                                <div class="card-header user-header alt bg-dark">
-                                    <div class="media">
-                                        <a href="#">
-                                            <img class="img-thumbnail mr-2" style="width:85px; height:85px;" alt="" src="<?php echo base_url('assets/img/calon/'.$foto)?>">
-                                        </a>
-                                        <div class="media-body">
-                                            <h4 class="text-light"><?php echo ucwords($namacalon);?></h2>
-                                            <p>Calon No <?php echo $no;?></p>
-                                        </div>
-                                    </div>
+    <?php 
+    $nototal                =0;
+    $belummemilih           =0;
+    $sudahmemilih           =0;
+    $sudahabsen             =0;
+    $belumabsen             =0;
+    $sudahabsenbelummilih   =0;
+        foreach($datapemilih->result_array() as $j):
+            $id=$j['id'];
+            $suara=$j['suara'];
+            $absen=$j['absen'];
+        if ($suara!=0) {
+            $sudahmemilih++;
+        }
+        if ($suara==0) {
+            $belummemilih++;
+        }
+        if ($absen!=0){
+            $sudahabsen++;
+        }
+        if ($absen==0) {
+            $belumabsen++;
+        }
+        if ($suara==0 && $absen!=0) {
+            $sudahabsenbelummilih++;
+        };
+        $nototal++;
+        endforeach;
+        
+    ?>
+        <div class="row">
+
+            <?php $no=1;
+                foreach($data->result_array() as $i):
+                    $id=$i['id'];
+                    $visi=$i['visi'];
+                    $misi=$i['misi']; 
+                    $namacalon=$i['namacalon']; 
+                    $foto=$i['foto']; 
+                    $totalsuara=$i['totalsuara'];        
+            ?>
+            <div class="col-lg-4">
+                <aside class="profile-nav alt">
+                    <section class="card">
+                        <div class="card-header user-header alt bg-dark">
+                            <div class="media">
+                                <a href="#">
+                                    <img class="img-thumbnail mr-2" style="width:85px; height:85px;" alt="" src="<?php echo base_url('assets/img/calon/'.$foto)?>">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="text-light"><?php echo ucwords($namacalon);?></h2>
+                                    <p>Calon No <?php echo $no;?></p>
                                 </div>
+                            </div>
+                        </div>
 
 
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">
-                                        <center>
-                                        <h3><br><?php echo $totalsuara;?> Suara</h3>
-                                        <h4><?php $persen=($totalsuara/$nototal)*100 ;echo $persen; ?> %</h4>
-                                        <br><br>
-                                    </center>
-                                    </li>
-                                </ul>
-                            </section>
-                        </aside>
-                    </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <center>
+                                <h3><br><?php echo $totalsuara;?> Suara</h3>
+                                <h4><?php $persen=($totalsuara/$nototal)*100 ;echo $persen; ?> %</h4>
+                                <br><br>
+                            </center>
+                            </li>
+                        </ul>
+                    </section>
+                </aside>
+            </div>
 
+        <?php $no++; endforeach;?>
+        </div>
 
-                <?php $no++; endforeach;?>
-                </div>
-
-        </div> <!-- .content -->
-        <div class="clearfix"></div>
+    </div> <!-- .content -->
+<div class="clearfix"></div>
 
 
 <!--Modal Keluar -->
-        <div class="modal fade" id="konfirmkeluar" tabindex="-1" role="dialog" aria-labelledby="staticModalLabel" style="display: none;" aria-hidden="true">
-                <div class="modal-dialog modal-sm" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="staticModalLabel">Apakah anda yakin ingin keluar?</h5>
-                        </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
-                        <form  action="<?php echo base_url('index.php/Welcome/logout'); ?>">
-                            <input type="submit" class="btn btn-primary" value="Ya">
-                        </form>
-                    </div>
+<div class="modal fade" id="konfirmkeluar" tabindex="-1" role="dialog" aria-labelledby="staticModalLabel" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog modal-sm" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticModalLabel">Apakah anda yakin ingin keluar?</h5>
                 </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
+                <form  action="<?php echo base_url('index.php/Welcome/logout'); ?>">
+                    <input type="submit" class="btn btn-primary" value="Ya">
+                </form>
             </div>
         </div>
+    </div>
+</div>
 
 <!--Modal Grafik -->
 <div class="modal fade" id="lihatgrafik" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
