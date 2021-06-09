01e43312e8ef9d3e00bfe3725e858781bb5a4390
yhaae01 <suryha7fold@gmail.com>
2021-06-10 03:24:31 +0700

    mengubah base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/e-pemilos/';
+$config['base_url'] = 'http://localhost/pemira/';
 
 
 /*
